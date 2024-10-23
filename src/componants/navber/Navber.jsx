a64f5950d0260aca44d7be56ee7e4b0c44819251
Jubayer husain <jubayerhusai360@gmail.com>
2024-10-23 08:13:33 +0600

    navber done

---
+++ src/componants/navber/Navber.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Navber = () => {
+    return (
+        <div className='flex justify-between w-10/12 mx-auto'>
+            <div>
+                <h1 className='font-bold text-4xl text-gray-800'>Knowledge Cafe</h1>
+            </div>
+            <div>
+                <img className='w-20 h-20' src="../../assets/profile.png" alt="profile picture hera" />
+            </div>
+        </div>
+    );
+};
+
+export default Navber;
